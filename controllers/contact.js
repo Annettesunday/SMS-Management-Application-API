4512e3906a9f4773c4b75c4c8b6d5010cab57b41
annettesunday <annette.odhiambo@andela.com>
2019-01-24 18:33:46 +0300

    ch(controllers): add dummy controllers

---
+++ controllers/contact.js
@@ -0,0 +1,30 @@
+class contactController {
+  createContact(req,res) {
+    return res.status(200)
+  }
+
+  getAContact(req,res) {
+    return res.status(200)
+  }
+
+  getAllContact(req,res) {
+    return res.status(200)
+  }
+  getAllSentMessages(req,res) {
+    res.status(200);
+  }
+
+  getAllReceivedMessages(req,res) {
+    res.status(200);
+  }
+
+  updateContact(req,res) {
+    return res.status(200)
+  }
+
+  deleteContact(req,res) {
+    return res.status(200)
+  }
+
+}
+export default contactController;
---
+++ controllers/message.js
@@ -0,0 +1,24 @@
+class messageController
+{
+  createMessage(req,res) {
+    res.status(200);
+  }
+
+  getAMessage(req,res) {
+    res.status(200);
+  }
+
+  getAllMessage(req,res) {
+    res.status(200);
+  }
+
+  updateMessage(req,res) {
+    res.status(200);
+  }
+
+  deleteMessage(req,res) {
+    res.status(200);
+  }
+
+}
+export default messageController;
